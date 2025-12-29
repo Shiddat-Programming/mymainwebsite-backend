e46b303e969cb57710c215996a2e6cc60c07fd5d
Shiddat-Programming <shiddatprogrammingindia@gmail.com>
2025-12-29 21:12:05 +0530

    done

--- src/config/mailConfig.js
+++ src/config/mailConfig.js
@@ -1,13 +1,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,      // smtp.hostinger.com
-  port: process.env.EMAIL_PORT,      // 465
-  secure: true,                      // SSL
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT), // MUST be number
+  secure: false, // TLS (NOT SSL)
   auth: {
-    user: process.env.EMAIL_USER,    // hr@witxtechnologies.com
-    pass: process.env.EMAIL_PASS     // email password
-  }
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // IMPORTANT for cloud
+  },
 });
 
 module.exports = transporter;
